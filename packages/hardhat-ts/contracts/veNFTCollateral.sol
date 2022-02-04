a0533f73d3c44f7b08dce80dd9456048af97deb1
viraj124 <virajm72@gmail.com>
2022-02-04 22:10:58 +0530

    chore: added contract template

---
+++ packages/hardhat-ts/contracts/veNFTCollateral.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract veNFTCollateral is ERC20 {
+  IERC20 public lendingToken;
+
+  constructor(IERC20 _lendingToken) ERC20("Reward", "REWARD") {
+    lendingToken = _lendingToken;
+  }
+
+  function deposit() external {}
+
+  function borrow() external {}
+
+  function claimDeposit() external {}
+
+  function repay() external {}
+}
