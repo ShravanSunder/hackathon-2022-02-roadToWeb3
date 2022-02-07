af47b325bfbfec937be6b3b1e2ad35f580e904b1
Mark Grothe <grothem@gmail.com>
2022-02-06 19:05:12 -0600

    Added a spot for lender to claim

--- packages/vite-app-ts/src/components/pages/lend/Lend.tsx
+++ packages/vite-app-ts/src/components/pages/lend/Lend.tsx
@@ -3,8 +3,8 @@ import { transactor } from 'eth-components/functions';
 import { EthComponentsSettingsContext } from 'eth-components/models';
 import { useContractReader, useGasPrice } from 'eth-hooks';
 import { useEthersContext } from 'eth-hooks/context';
-import { utils } from 'ethers';
-import { FC, useContext, useState } from 'react';
+import { BigNumberish, utils } from 'ethers';
+import { FC, useContext, useEffect, useState } from 'react';
 
 import { useAppContracts } from '~~/config/contractContext';
 
@@ -22,6 +22,28 @@ export const Lend: FC = (props) => {
   const [amount, setAmount] = useState(0);
   const [duration, setDuration] = useState(0);
   const [collateralRatio, setCollateralRatio] = useState(0);
+
+  const [numberOfLoans] = useContractReader(deNFT, deNFT?.totalNumLoans);
+  const [myLoans, setMyLoans] = useState<any[]>([]);
+  useEffect(() => {
+    const getLoans = async (): Promise<void> => {
+      const myLoans: any[] = [];
+      if (!numberOfLoans) return;
+      for (let i = 0; i < numberOfLoans.toNumber(); i++) {
+        const loan = await deNFT?.loanIdToLoan(i);
+        if (!loan) continue;
+
+        if (loan.lender === ethersContext.account) {
+          myLoans.push(loan);
+        }
+      }
+      setMyLoans(myLoans);
+    };
+    getLoans().then(
+      () => {},
+      () => {}
+    );
+  }, [numberOfLoans, ethersContext.account]);
 
   return (
     <>
@@ -77,6 +99,22 @@ export const Lend: FC = (props) => {
             }}>
             Deposit
           </button>
+        </div>
+        <div>
+          {myLoans?.map((loan, i) => (
+            <div key={i} className="flex items-center justify-around m-1">
+              <div>id: {loan.loanId.toNumber()}</div>
+              <div>Duration: {loan.loanDuration} seconds</div>
+              <div>Amount lent: {utils.formatEther(loan.borrowedAmount as BigNumberish)}</div>
+              <button
+                className="btn btn-secondary btn-sm"
+                onClick={async (): Promise<void> => {
+                  await tx?.(deNFT?.claimDeposit(loan.loanId as BigNumberish));
+                }}>
+                Claim
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </>
